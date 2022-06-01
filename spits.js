698f6cf2d3f065bc1c77e4e89eacc033f945293d
Ryokuman <ryokuman@naver.com>
2022-06-01 21:12:55 +0900

    s

--- spits.js
+++ spits.js
@@ -3,6 +3,6 @@ const Item = class {
     block;
     constructor(time, block) {
         this.block = block;
-        this.time = time + performance.now();
+        this.time = time + performance.now(); // date.now 에 비하여 나노 초도 볼 수 있다.
     }
 };
